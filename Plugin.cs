d53150f22938824ca7c4aff63f1ddef96fd3e176
리펙스 <66472285+xoals35@users.noreply.github.com>
2024-01-04 15:18:50 +0900

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,12 +1,15 @@
-﻿using Exiled.API.Enums;
+using Exiled.API.Enums;
 using Log = Exiled.API.Features.Log;
 using ServerEvents = Exiled.Events.Handlers.Server;
 using PlayerEvents = Exiled.Events.Handlers.Player;
 using MapEvents = Exiled.Events.Handlers.Map;
 using WarheadEvents = Exiled.Events.Handlers.Warhead;
+using Handlers = Exiled.Events.Handlers;
 using Features = Exiled.API.Features;
 using Exiled.Events;
 using PeutiPlugin;
+using System;
+using System.Runtime.InteropServices;
 
 namespace PeutiPlugin
 {
@@ -14,19 +17,31 @@ namespace PeutiPlugin
     {
         public EventHandlers EventHandlers { get; private set; }
 
-        public override string Name { get; } = "PeutiBroadcastPlugin";
-        public override string Prefix { get; } = "PeutiBroadcastPlugin";
-        public override string Author { get; } = "Peuti";
+        public override string Name { get; } = "프티(편의성플러그인)";
+        public override string Prefix { get; } = "편의성플러그인";
+        public override string Author { get; } = "@Peuti";
+        public override Version Version { get; } = new Version(1, 0, 1);
+        public override Version RequiredExiledVersion { get; } = new Version(8, 5, 0);
 
         public override PluginPriority Priority { get; } = PluginPriority.Default;
 
         public void LoadEvents()
         {
-            ServerEvents.RoundStarted += EventHandlers.OnRoundStarting;
             MapEvents.AnnouncingDecontamination += EventHandlers.OnAnnouncingDecontamination;
             MapEvents.Decontaminating += EventHandlers.OnDecontaminating;
             MapEvents.AnnouncingNtfEntrance += EventHandlers.OnAnnouncingNtfEntrance;
-           
+            PlayerEvents.Dying += EventHandlers.OnPlayerDeath;
+            Handlers.Scp079.InteractingTesla += EventHandlers.On079TeslaEvent;   
+            ServerEvents.RoundEnded += EventHandlers.OnRoundEnded;
+            Handlers.Scp096.AddingTarget += EventHandlers.On096AddTarget;
+            ServerEvents.RespawningTeam += EventHandlers.OnTeamRespawn;
+            PlayerEvents.Handcuffing += EventHandlers.OnPlayerHandcuff;
+            PlayerEvents.RemovingHandcuffs += EventHandlers.OnPlayerUnhandCuff;
+            MapEvents.AnnouncingScpTermination += EventHandlers.OnAnnouncingScpTermination;
+            PlayerEvents.Left += EventHandlers.OnScpLeave;
+            ServerEvents.RoundStarted += EventHandlers.OnRoundStarting;
+
+
         }
 
         public override void OnEnabled()
@@ -36,19 +51,30 @@ namespace PeutiPlugin
             base.OnEnabled();
 
             EventHandlers = new EventHandlers(this);
-            LoadEvents();
-            Log.Info("브로드캐스트 플러그인 활성화");
+            LoadEvents();   
+            Log.Info("플러그인 활성화");
         }
 
         public override void OnDisabled()
         {
             base.OnDisabled();
 
-            ServerEvents.RoundStarted -= EventHandlers.OnRoundStarting;
             MapEvents.AnnouncingDecontamination -= EventHandlers.OnAnnouncingDecontamination;
             MapEvents.Decontaminating -= EventHandlers.OnDecontaminating;
             MapEvents.AnnouncingNtfEntrance -= EventHandlers.OnAnnouncingNtfEntrance;
-            
+            PlayerEvents.Dying += EventHandlers.OnPlayerDeath;
+            Handlers.Scp079.InteractingTesla += EventHandlers.On079TeslaEvent;
+            ServerEvents.RoundEnded += EventHandlers.OnRoundEnded;
+            Handlers.Scp096.AddingTarget += EventHandlers.On096AddTarget;
+            ServerEvents.RespawningTeam += EventHandlers.OnTeamRespawn;
+            PlayerEvents.Handcuffing += EventHandlers.OnPlayerHandcuff;
+            PlayerEvents.RemovingHandcuffs += EventHandlers.OnPlayerUnhandCuff;
+            MapEvents.AnnouncingScpTermination += EventHandlers.OnAnnouncingScpTermination;
+            PlayerEvents.Left -= EventHandlers.OnScpLeave;
+            ServerEvents.RoundStarted -= EventHandlers.OnRoundStarting;
+
+
+
             EventHandlers = null;
         }
 
