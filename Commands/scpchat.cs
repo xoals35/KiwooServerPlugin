dc88bf23cb9961b2e0ad01bfd99cbb10b5adc6bc
리펙스 <66472285+xoals35@users.noreply.github.com>
2024-01-09 22:16:57 +0900

    Update scpchat.cs

--- Commands/scpchat.cs
+++ Commands/scpchat.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -41,7 +41,7 @@ namespace PeutiPlugin
 
             if (player.Role.Team != Team.SCPs)
             {
-                response = "당신은 SCP진영이 아닙니다.";
+                response = "당신은 SCP진영이 아닙니다.(플라밍고 제외)";
                 return true;
             }
             string content = $"<color=red>[SCPCHAT]</color>{player.Nickname}({player.Role.Name})\n 메세지 내용:";
